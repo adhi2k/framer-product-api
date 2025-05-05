df3e107485bfe8cc8251c90f7162e3f31656d3c1
Adhithya K <145216997+adhi2k@users.noreply.github.com>
2025-05-05 11:52:45 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,28 +3,21 @@ import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 const products = [
   {
     name: "Adhithya",
-    slug: "adhi-twestern-arkansas-button-up-dark-hash-floral",
-    description: "adhi Experience a unique Western style...",
     price: 108,
-    compareAtPrice: 0,
-    category: "Home page",
-    tags: ["Aglini", "Cotton", "Western", "SALE"],
-    productType: "Women's button-ups"
+    stock: 10
   },
   {
-    name: "Young Man in Vibrant Jacket",
-    slug: "western-arkansas-button-up-blue-floral",
-    description: "A Western style shirt gets a sweet touch...",
-    price: 108,
-    compareAtPrice: 147,
-    category: "Winter",
-    tags: ["Aglini", "Cotton", "Western", "SALE"],
-    productType: "Men's button-ups"
+    name: "Young Man",
+    price: 120,
+    stock: 5
   }
 ];
 
 serve((_req) =>
   new Response(JSON.stringify(products), {
-    headers: { "Content-Type": "application/json" }
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"  // <-- REQUIRED for Framer!
+    }
   })
 );
